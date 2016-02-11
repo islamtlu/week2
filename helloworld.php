0187e413308fb4855c6620a4cc8b3b23978b708c
islmuh <islmuh@greeny.cs.tlu.ee>
2016-02-11 12:42:47 +0200

    added first lines of php

---
+++ helloworld.php
@@ -0,0 +1,14 @@
+<?php
+
+	// Variable starts with $,
+	// string has to use quotation marks
+	// line has to end with semicolon
+	$first_name = "Islam";
+	$last_name = "Muhammad";
+	
+	// show code to browser
+	// for adding up numbers, use + sign
+	// for adding up strings, use .
+	echo $first_name." ".$last_name;
+
+?>
